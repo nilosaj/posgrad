a6f67de0b0d8611a69e849ba7ce431782cc1d207
unknown <nilosergio.assisjr@ymail.com>
2017-06-14 00:58:46 -0300

    progressao no trabalho

--- futebol_bigdata/app.js
+++ futebol_bigdata/app.js
@@ -2,4 +2,14 @@ var futebol = require("./soccer")
 
 //futebol.TabelaJogos('2008/2009')
 
-futebol.pontuacaoFinal('2008/2009',1729)
+futebol.consultaPontuacaoFinal('2008/2009',1729)
+futebol.consultaPontuacaoFinal('2009/2010',1729)
+futebol.consultaPontuacaoFinal('2010/2011',1729)
+futebol.consultaPontuacaoFinal('2011/2012',1729)
+futebol.consultaPontuacaoFinal('2012/2013',1729)
+futebol.consultaPontuacaoFinal('2013/2014',1729)
+futebol.consultaPontuacaoFinal('2014/2015',1729)
+futebol.consultaPontuacaoFinal('2015/2016',1729)
+
+
+//futebol.capturaInfoCasa(10260,1729,'2008/2009')
--- futebol_bigdata/soccer.js
+++ futebol_bigdata/soccer.js
@@ -1,7 +1,8 @@
 var  sqlite3 = require('sqlite3').verbose();
-
+var async = require('async');
 var dbfutebol = new sqlite3.Database('database.sqlite');
-
+var xpath = require('xpath');
+var dom = require('xmldom').DOMParser;
 
 
 exports.TabelaJogos = function consultaTabelaJogos(temporada){
@@ -25,44 +26,157 @@ exports.TabelaJogos = function consultaTabelaJogos(temporada){
 
 
 
-exports.pontuacaoFinal = function consultaPontuacaoFinal(temporada,idPais){
+exports.consultaPontuacaoFinal = function consultaPontuacaoFinal(temporada,idPais){
     dbfutebol.serialize(function(){
-        dbfutebol.each(
-    "    select  Tabela.home_team_api_id as teamId , Team.team_long_name as teamName, Tabela.score as score from   "+
-    "     (select H.home_team_api_id , H.home+A.Away as score  from     "+
-    "         (select home_team_api_id , sum(point_home) as home from   "+
-    "             (  select  id , country_id , league_id , season , stage , date , match_api_id , home_team_api_id , away_team_api_id ,home_team_goal , away_team_goal ,    "+
-    "              CASE WHEN home_team_goal > away_team_goal THEN 3      "+
-    "                   WHEN home_team_goal < away_team_goal THEN 0      "+
-    "                   WHEN home_team_goal == away_team_goal THEN 1     "+
-    "                   END AS 'point_home'  from    "+
-    "                     Match where season = '2008/2009' and league_id = 1729 ) group by home_team_api_id  ) H ,   "+
-    "         (select away_team_api_id , sum(point_away) as away from    "+
-    "             (  select  id , country_id , league_id , season , stage , date , match_api_id , home_team_api_id , away_team_api_id ,home_team_goal , away_team_goal ,  "+
-    "              CASE WHEN home_team_goal < away_team_goal THEN 3     "+
-    "                   WHEN home_team_goal > away_team_goal THEN 0   "+
-    "                   WHEN home_team_goal == away_team_goal THEN 1      "+
-    "                   END AS 'point_away'   from     "+
-    "                      Match where season = '2008/2009' and league_id = 1729  ) group by away_team_api_id  ) A    "+
-    "      on H.home_team_api_id = A.away_team_api_id  ) as Tabela , Team  on Tabela.home_team_api_id = Team.team_api_id    "+
-    "      order by Tabela.score desc        "
+        dbfutebol.all(
+    "  select  Tabela.home_team_api_id as teamId, Team.team_long_name as teamName, Tabela.score as score ,Tabela.Home_shootsIn as home_shoots,Tabela.Home_cross ,Tabela.Home_possession,Tabela.Home_corner ,Tabela.Away_shootsIn,Tabela.Away_cross , Tabela.Away_possession,Tabela.Away_corner   from   "+
+    "  (select H.home_team_api_id , H.home+A.Away as score , H.shoton as Home_shootsIn,H.cross as Home_cross , H.possession as Home_possession,H.corner as Home_corner , A.shoton as Away_shootsIn,A.cross as Away_cross , A.possession as Away_possession,A.corner as Away_corner    from     "+
+    "      (select home_team_api_id , shoton ,cross , possession,corner , sum(point_home) as home from  "+
+    "          (select  id , country_id , league_id , season , stage , date , match_api_id , home_team_api_id , away_team_api_id ,home_team_goal , away_team_goal, shoton ,cross , possession,corner ,    "+
+    "           CASE WHEN home_team_goal > away_team_goal THEN 3      "+
+    "                WHEN home_team_goal < away_team_goal THEN 0      "+
+    "                WHEN home_team_goal == away_team_goal THEN 1     "+
+    "                END AS 'point_home'  from    "+
+    "                  Match where season = '"+temporada+"' and league_id = 1729 ) group by home_team_api_id  ) H ,   "+
+    "      (select away_team_api_id , shoton ,cross , possession,corner, sum(point_away) as away from    "+
+    "          (  select  id , country_id , league_id , season , stage , date , match_api_id , home_team_api_id , away_team_api_id ,home_team_goal , away_team_goal ,  shoton,cross , possession,corner ,    "+
+    "           CASE WHEN home_team_goal < away_team_goal THEN 3     "+
+    "                WHEN home_team_goal > away_team_goal THEN 0     "+
+    "                WHEN home_team_goal == away_team_goal THEN 1    "+  
+    "                END AS 'point_away'   from     "+
+    "                   Match where season = '"+temporada+"' and league_id = "+idPais+"  ) group by away_team_api_id  ) A    "+
+    "   on H.home_team_api_id = A.away_team_api_id  ) as Tabela , Team  on Tabela.home_team_api_id = Team.team_api_id   "+    
+    "   order by Tabela.score desc     "
 
 
 
 
-            ,function(error,row){
+            ,function(error,rows){
                 //console.log("["+row.teamId+"]   "+row.teamName+"    ("+row.tabela.score+")")
-                console.log("["+row.teamId+"]   " + row.teamName + "  ("+row.score+")" )
+                //console.log("["+row.teamId+"]   " + row.teamName + "  ("+row.score+")" + " "+capturaChutes(row.home_shoots,row.teamId) )
+                //console.log(row.length);
+                var times = [];
+                async.eachSeries(rows,function(teste,callback){
+                    rows.forEach(function(row){
+                                var saida = capturaInfoCasa(row.teamId,row.teamName,row.score,idPais,temporada);
+                                
+                               // console.log("["+row.teamId+"]   " + row.teamName + "  ("+row.score+")  ") 
+                    });  
+                });  
+
+
+
+
+
             } )
 
 
     });
-     dbfutebol.close();
+    
 }
 
 
 
+ function capturaInfoCasa(teamId ,teamName, score ,idPais,season){
+    var chutes =0;
+    var cruzamentos =0;
+    var escanteios =0;
+    var posse = 0;
+    
+    dbfutebol.serialize(function(){
+        dbfutebol.all("select shoton,cross , possession ,corner  from Match where league_id = "+idPais+"  and home_team_api_id ="+teamId+" and season = '"+season+"' ",function(err,rows){
+           if(err){
+               console.log(err)
+           }
 
+            async.eachSeries(rows,function(teste,callback){
 
+                     rows.forEach(function(row){
+                         
+                                var docshot = new dom().parseFromString(row.shoton);
+                                var shot = xpath.select("sum(/shoton/value/stats/shoton)", docshot);
+                                chutes = chutes + shot;
+                                
+                                var doccross = new dom().parseFromString(row.cross);
+                                var  cross =+ xpath.select("sum(/cross/value/stats/crosses)", doccross);
+                                cruzamentos= cruzamentos +cross;
+                                
+                                var docposse = new dom().parseFromString(row.possession);
+                                var possession =+ xpath.select("(/possession/value[elapsed='90']/homepos/text())", docposse);
+                                
+                                if (possession > 0) {
+                                    posse = posse + possession
+                                }else{        
+                                    posse = posse + 50;
+                                }
+                                var doccorner = new dom().parseFromString(row.corner);
+                                var corner =+ xpath.select("sum(/corner/value/stats/corners)", doccorner);
+                                escanteios = escanteios + corner;
+                            // console.log("["+teamId+"] chutes="+shot )//+"  cruzamentos="+cross+"  posse="+possession+" escanteio="+corner )
+                                
+                            //console.log("quantidade atual de -  chutes :"+chutes+"  cruzamentos:"+cruzamentos +" escanteios:"+escanteios+"  posse:"+(posse))
 
+                           
+                        });
+                         var valores = "CASA"+idPais+";"+season+";"+teamId+";"+teamName+";"+score+";"+chutes+";"+cruzamentos+";"+Math.round(posse/18)+";"+escanteios
+                            console.log(valores) 
 
+                               
+            })
+                       
+        })    
+    })
+  
+}
+
+function capturaInfoCasa(teamId ,teamName, score ,idPais,season){
+    var chutes =0;
+    var cruzamentos =0;
+    var escanteios =0;
+    var posse = 0;
+    
+    dbfutebol.serialize(function(){
+        dbfutebol.all("select shoton,cross , possession ,corner  from Match where league_id = "+idPais+"  and away_team_api_id ="+teamId+" and season = '"+season+"' ",function(err,rows){
+           if(err){
+               console.log(err)
+           }
+
+            async.eachSeries(rows,function(teste,callback){
+
+                     rows.forEach(function(row){
+                         
+                                var docshot = new dom().parseFromString(row.shoton);
+                                var shot = xpath.select("sum(/shoton/value/stats/shoton)", docshot);
+                                chutes = chutes + shot;
+                                
+                                var doccross = new dom().parseFromString(row.cross);
+                                var  cross =+ xpath.select("sum(/cross/value/stats/crosses)", doccross);
+                                cruzamentos= cruzamentos +cross;
+                                
+                                var docposse = new dom().parseFromString(row.possession);
+                                var possession =+ xpath.select("(/possession/value[elapsed='90']/homepos/text())", docposse);
+                                
+                                if (possession > 0) {
+                                    posse = posse + possession
+                                }else{        
+                                    posse = posse + 50;
+                                }
+                                var doccorner = new dom().parseFromString(row.corner);
+                                var corner =+ xpath.select("sum(/corner/value/stats/corners)", doccorner);
+                                escanteios = escanteios + corner;
+                            // console.log("["+teamId+"] chutes="+shot )//+"  cruzamentos="+cross+"  posse="+possession+" escanteio="+corner )
+                                
+                            //console.log("quantidade atual de -  chutes :"+chutes+"  cruzamentos:"+cruzamentos +" escanteios:"+escanteios+"  posse:"+(posse))
+
+                           
+                        });
+                         var valores = "CASA"+idPais+";"+season+";"+teamId+";"+teamName+";"+score+";"+chutes+";"+cruzamentos+";"+Math.round(posse/18)+";"+escanteios
+                            console.log(valores) 
+
+                               
+            })
+                       
+        })    
+    })
+  
+}
